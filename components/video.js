4c6ae422024a3de3e8da6b4b63a75cd0f44dc27b
m.lotfi <m.lotfi@pinket.com>
2022-08-03 23:39:56 +0430

    Complete main page

--- components/video.js
+++ components/video.js
@@ -1,27 +1,93 @@
 import "video-react/dist/video-react.css";
 import {
-    Player,
     BigPlayButton,
     ControlBar,
     ForwardControl,
-    LoadingSpinner, PlaybackRateMenuButton,
+    LoadingSpinner,
+    PlaybackRateMenuButton,
+    Player,
     PlayToggle,
     ReplayControl,
     VolumeMenuButton
 } from "video-react";
+import Link from "next/link";
+import {Badge, Card} from "react-bootstrap";
+import {css} from "@emotion/react";
+import {Component, useEffect, useState} from "react";
 
 export function VideoPlayer(props) {
     return (
         <Player {...props}>
-            <BigPlayButton position="center" />
-            <LoadingSpinner />
+            <BigPlayButton position="center"/>
+            <LoadingSpinner/>
             <ControlBar autoHide>
-                <ReplayControl seconds={10} order={1.1} />
-                <PlayToggle order={1.2} />
-                <ForwardControl seconds={30} order={1.3} />
-                <VolumeMenuButton vertical />
-                <PlaybackRateMenuButton rates={[2, 1.5, 1.25, 1, 0.75, 0.5]} order={6.1} />
+                <ReplayControl seconds={10} order={1.1}/>
+                <PlayToggle order={1.2}/>
+                <ForwardControl seconds={30} order={1.3}/>
+                <VolumeMenuButton vertical/>
+                <PlaybackRateMenuButton rates={[2, 1.5, 1.25, 1, 0.75, 0.5]} order={6.1}/>
             </ControlBar>
         </Player>
     )
+}
+
+export class LightVideoPlayer extends Component {
+    componentDidMount() {
+        this.player.subscribeToStateChange(this.handleStateChange.bind(this));
+        this.player.muted=true;
+        this.player.playbackRate = 2;
+    }
+
+    handleStateChange(state) {
+        this.setState({
+            player: state
+        });
+    }
+
+    render() {
+        return (
+            <Player {...this.props} ref={player => {this.player = player;}} autoPlay>
+                <ControlBar autoHide disableDefaultControls/>
+            </Player>
+        );
+    }
+}
+
+export function VideoCard({video, className}) {
+    const [mouseEntered, setMouseEntered] = useState(false);
+    const [showVideo, setShowVideo] = useState(false);
+
+    useEffect(() => {
+        let timeout;
+        if (mouseEntered) {
+            timeout = setTimeout(() => setShowVideo(true), 1000);
+        }
+        return () => {
+            if (mouseEntered) {
+                clearTimeout(timeout);
+                setShowVideo(false);
+            }
+        }
+    }, [mouseEntered]);
+
+    return (
+        <Link href={`/${video.uid}`}>
+            <Card className="w-100" onMouseEnter={() => setMouseEntered(true)}
+                  onMouseOut={() => setMouseEntered(false)}>
+                {showVideo ? <LightVideoPlayer src={`/api/videos/${video.uid}/stream`}/> :
+                    <Card.Img variant="top" src={`/api/videos/${video.uid}/poster`}/>}
+                <Badge css={css`width: 45px;
+                  margin-top: -25px;
+                  margin-left: 5px`} bg="light" text="dark">
+                    {Math.floor(video.duration / 60)}:{video.duration % 60}
+                </Badge>
+                <Card.Body>
+                    <Card.Title>{video.title}</Card.Title>
+                    <Card.Text>
+                        {video.description}
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        </Link>
+    );
 }
--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,9 @@ import Head from 'next/head'
 import Layout from "../components/layout/user";
 import {useContext, useEffect, useState} from "react";
 import InfiniteScroll from "react-infinite-scroll-component";
-import {Badge, Card, Row} from "react-bootstrap";
 import axios from "axios";
 import SearchContext from "../context/search";
-import Link from "next/link";
-import {css} from "@emotion/react";
+import {VideoCard} from "../components/video";
 
 const VIDEOS_PAGE_SIZE = 20;
 
@@ -43,30 +41,13 @@ export default function Home() {
                 <link rel="icon" href="/favicon.ico"/>
             </Head>
             <InfiniteScroll
-                className="overflow-hidden"
+                className="videos-container"
                 dataLength={videos.length}
                 next={getMoreVideos}
                 hasMore={hasMoreVideos}
-                loader={<h6>Loading comments ...</h6>}
+                loader={<h6>Loading ...</h6>}
             >
-                <Row>
-                    {videos.map(video => (
-                        <Link key={video.uid} href={`/${video.uid}`}>
-                            <Card className="col-4 p-2 border-0 cursor-pointer">
-                                <Card.Img variant="top" src={`/api/videos/${video.uid}/poster`}/>
-                                <Badge css={css`width: 45px; margin-top: -25px; margin-left: 5px`} bg="light" text="dark">
-                                    {Math.floor(video.duration / 60)}:{video.duration % 60}
-                                </Badge>
-                                <Card.Body>
-                                    <Card.Title>{video.title}</Card.Title>
-                                    <Card.Text>
-                                        {video.description}
-                                    </Card.Text>
-                                </Card.Body>
-                            </Card>
-                        </Link>
-                    ))}
-                </Row>
+                    {videos.map(video => <VideoCard key={video.uid} video={video}/>)}
             </InfiniteScroll>
         </Layout>
     )
